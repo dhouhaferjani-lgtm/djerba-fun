6e958169f4c28eed16d6069969dfdbb260a7c2af
Dhouha <dhouha.ferjani@otospex.com>
2026-02-03 16:39:01 +0100

    Revert "feat(web): add header and footer to checkout pages"
    This reverts commit c72ee197b3aeb75d80cbbfa0303a0ed0ddbc5e32.

--- apps/web/src/app/[locale]/checkout/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { MainLayout } from '@/components/templates/MainLayout';
-import { useLocale } from 'next-intl';
-
-export default function CheckoutLayout({ children }: { children: React.ReactNode }) {
-  const locale = useLocale();
-  return <MainLayout locale={locale}>{children}</MainLayout>;
-}
